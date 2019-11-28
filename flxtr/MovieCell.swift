4ecf705b18a8855d21f08038894d706609fe894a
dnlfmkn <famakindaniel7@gmail.com>
2019-11-28 01:46:10 -0500

    basic flixter implemented

---
+++ flxtr/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  flxtr
+//
+//  Created by user160656 on 11/28/19.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var detailsLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
