ac0162af297c725c869860804d6bf60a2d08a688
dnlfmkn <famakindaniel7@gmail.com>
2019-12-07 00:21:10 -0500

    Added tab bars and details screen to flxtr

---
+++ flxtr/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flxtr
+//
+//  Created by user160656 on 12/6/19.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- flxtr/MoviesViewController.swift
+++ flxtr/MoviesViewController.swift
@@ -54,14 +54,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//         Get the new view controller using segue.destination.
+//         Pass the selected object to the new view controller.
+        let cell = sender as! UITableViewCell
+        let indexPath = moviesTableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        moviesTableView.deselectRow(at: indexPath, animated: true)
     }
-    */
 
 }
